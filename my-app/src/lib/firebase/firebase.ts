d4b91ce50e941dff090cb5c37864842572b8113d
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-10-22 01:30:05 +0900

    edit

--- my-app/src/lib/firebase/firebase.ts
+++ my-app/src/lib/firebase/firebase.ts
@@ -10,7 +10,9 @@ import {
   FIREBASE_MESSAGING_SENDER_ID,
   FIREBASE_PROJECT_ID,
   FIREBASE_STORAGE_BUCKET,
+  FIREBASE_DATABASE_URL,
 } from '@/constant/env'
+import firebase from 'firebase/compat/app'
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
@@ -19,10 +21,17 @@ const firebaseConfig = {
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   appId: FIREBASE_APP_ID,
+  databaseURL: FIREBASE_DATABASE_URL,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-export const initializeFirebaseApp = () =>
+export const initializeFirebaseApp = () => {
+  // if (!firebase.apps.length) {
+  //   firebase.initializeApp({})
+  // } else {
+  //   firebase.app()
+  // }
   !getApps().length ? initializeApp(firebaseConfig) : getApp()
+}
