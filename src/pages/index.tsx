9f9f82066e84a2c10a3a71300f385ede8a3c7603
FromBanana <ndomusicchannel@gmail.com>
2023-10-25 12:59:48 +0900

    aaa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import Header from "@/component/Header";
 import Footer from "@/component/Footer";
 
 export default function Home() {
+  const try = "hi";
   console.log(getApp());
 
   return (
@@ -26,3 +27,4 @@ export default function Home() {
     </>
   );
 }
+
