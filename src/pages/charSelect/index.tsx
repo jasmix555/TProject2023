6844788d94b5502c36cb8694d448577f680dc052
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-11-16 11:24:33 +0900

    Add character carousel functionality to charSelect page

--- src/pages/charSelect/index.tsx
+++ src/pages/charSelect/index.tsx
@@ -1,21 +1,65 @@
+import { useState } from "react";
 import LayoutPage from "@/component/LayoutPage";
 import style from "@/styles/charSelect.module.scss";
+import Image from "next/image";
 
 export default function CharSelect() {
+  const [currentChar, setCurrentChar] = useState(1);
+  const totalChars = 5;
+
+  const goLeft = () => {
+    setCurrentChar(currentChar === 1 ? totalChars : currentChar - 1);
+  };
+
+  const goRight = () => {
+    setCurrentChar(currentChar === totalChars ? 1 : currentChar + 1);
+  };
+
   return (
     <LayoutPage>
       <div className={style.header}>
         <h1>アバター選択</h1>
       </div>
       <div className={style.carouselWrapper}>
-        <div className={style.characterWrap}></div>
+        <div className={style.characterWrap}>
+          <div className={style.previous}>
+            <Image
+              src={`/characters/Char${
+                currentChar === 1 ? totalChars : currentChar - 1
+              }M.svg`}
+              alt="previous character"
+              width={141}
+              height={200}
+              className={style.dimmed}
+            />
+          </div>
+          <div className={style.current}>
+            <Image
+              src={`/characters/Char${currentChar}L.svg`}
+              alt="current character"
+              width={283}
+              height={400}
+            />
+          </div>
+          <div className={style.next}>
+            <Image
+              src={`/characters/Char${
+                currentChar === totalChars ? 1 : currentChar + 1
+              }M.svg`}
+              alt="next character"
+              width={141}
+              height={200}
+              className={style.dimmed}
+            />
+          </div>
+        </div>
 
         <div className={style.carouselBtnWrapper}>
           <div className={style.left}>
-            <button></button>
+            <button onClick={goLeft}>Left</button>
           </div>
           <div className={style.right}>
-            <button></button>
+            <button onClick={goRight}>Right</button>
           </div>
         </div>
       </div>
