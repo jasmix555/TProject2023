44dead8f13cbddddcc8527878ad5b1100d8c3792
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-11-16 11:09:57 +0900

    Add character carousel to CharSelect page

--- src/pages/charSelect/index.tsx
+++ src/pages/charSelect/index.tsx
@@ -7,6 +7,18 @@ export default function CharSelect() {
       <div className={style.header}>
         <h1>アバター選択</h1>
       </div>
+      <div className={style.carouselWrapper}>
+        <div className={style.characterWrap}></div>
+
+        <div className={style.carouselBtnWrapper}>
+          <div className={style.left}>
+            <button></button>
+          </div>
+          <div className={style.right}>
+            <button></button>
+          </div>
+        </div>
+      </div>
     </LayoutPage>
   );
 }
